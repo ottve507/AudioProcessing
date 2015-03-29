5293a8ec535643d74613ef3b5c6e1bd641bbfdd0
Otto Velander <o.velander@gmail.com>
2015-03-29 18:09:20 +0200

    Hello Sine

--- src/Start.java
+++ src/Start.java
@@ -7,42 +7,21 @@ import net.beadsproject.beads.ugens.*;
 public class Start {
 	// create our AudioContext
 	static AudioContext ac;
-	static Gain masterGain;
+	static Gain g;
 	static RecordToSample rts;
 	// this object will hold our audio data
 	static Sample targetSample;
 	static boolean recording = false;
 
 	public static void main(String args[]) {
-		// initialize the AudioContext
 		ac = new AudioContext();
-		// get an AudioInput UGen from the AudioContext
-		// this will setup an input from whatever input is your
-		// default audio input (usually the microphone in)
-		UGen microphoneIn = ac.getAudioInput();
-		// setup the recording unit generator
-		try {
-			// setup a recording format
-			AudioFormat af = new AudioFormat(44100.0f, 16, 1, true, true);
-			// create a holder for audio data
-			targetSample = new Sample(44100);
-			// initialize the RecordToSample object
-			rts = new RecordToSample(ac, targetSample, RecordToSample.Mode.INFINITE);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		
-		// connect the microphone input to the RecordToSample
-		rts.addInput(microphoneIn);
-		// tell the AudioContext to work with the RecordToSample
-		ac.out.addDependent(rts);
-		// pause the RecordToSample object
-		rts.pause(true);
-		// set up our usual master gain object
-		masterGain = new Gain(ac, 1);
-		masterGain.addInput(microphoneIn);
-		
-		ac.out.addInput(masterGain);
+		// create a WavePlayer
+		// WavePlayer objects generate a waveform
+		WavePlayer wp = new WavePlayer(ac, 440, Buffer.SINE);
+		// connect the WavePlayer to the AudioContext
+		ac.out.addInput(wp);
+		// start audio processing
 
 		try {
 			ac.start();
